7c338a560ed3e5fa262712a7a99c82700f77ed33
Tu Ho <tu.hh@geekup.vn>
2022-08-24 21:20:29 +0700

    Frontend Layout and Modal

---
+++ frontend/src/app/home/components/start-post/modal/modal.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { ModalController } from '@ionic/angular';
+
+@Component({
+  selector: 'app-modal',
+  templateUrl: './modal.component.html',
+  styleUrls: ['./modal.component.scss'],
+})
+export class ModalComponent implements OnInit {
+  @ViewChild('form') form: NgForm;
+  constructor(public modalController: ModalController) {}
+
+  ngOnInit() {}
+
+  onDismiss() {
+    this.modalController.dismiss(null, 'dismiss');
+  }
+  onPost() {
+    if (!this.form.valid) return;
+    const body = this.form.value['body'];
+    this.modalController.dismiss(
+      {
+        post: {
+          body,
+          createAt: new Date(),
+        },
+      },
+      'post'
+    );
+  }
+}
