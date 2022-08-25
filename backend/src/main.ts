798e90dc608a674f51093f9fc7ad025388a94714
Tu Ho <tu.hh@geekup.vn>
2022-08-25 15:06:53 +0700

    Enable CORS

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   app.setGlobalPrefix('api'); //Prefix Api
   await app.listen(3000);
 }
